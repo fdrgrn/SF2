ed878bc2cf3ac6ddcf378e01e9c2ff7c2838c20f
fdrgrn <feodor.gornostayev@dawsoncollege.qc.ca>
2025-01-26 23:26:16 -0500

    Create streams.py

---
+++ week1/streams.py
@@ -0,0 +1,31 @@
+current_streams = int(input())
+stream_flow = []
+
+for i in range(current_streams):
+    flow = int(input())
+    stream_flow.append(flow)
+
+while True:
+    command = int(input())
+
+    #End of splits and joins for the streams
+    if command == 77:
+        break
+
+    #Split of streams 
+    elif command == 99:
+        pos_split = int(input())
+        split_flow = int(input())
+        inital_flow = stream_flow[pos_split - 1]
+        stream_flow[pos_split - 1] = inital_flow * split_flow // 100
+        stream_flow.insert(pos_split, inital_flow * (100 - split_flow) // 100)
+    
+    #Joining of streams
+    elif command == 88:
+        pos_join = int(input())
+        join_flow = stream_flow[pos_join - 1] + stream_flow[pos_join]
+        stream_flow[pos_join - 1] = join_flow
+        stream_flow.pop(pos_join)
+
+#Unpacking and print of streams
+print(*stream_flow)
